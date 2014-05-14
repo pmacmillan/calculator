193d8a9e6e0d57892ddec8c37c73ad68c133f193
Peter MacMillan <peter@macmillan.io>
2014-05-13 21:58:06 -0400

    look and feel

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 
 var http = require('http')
   , express = require('express')
+  , expressLess = require('express-less')
   , app = express()
   , server = http.createServer(app);
 
 
+app.use('/css', expressLess(__dirname + '/less'));
 app.get('/', function (req, res) {
   res.render('calculator.jade');
 });
