3f207202c41848238932dd67f09b0e6ad9475a02
Peter MacMillan <peter@macmillan.io>
2014-05-14 19:26:58 -0400

    moving things around

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.locals.pretty = true;
 
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', expressLess(__dirname + '/less'));
-app.get('/', function (req, res) { res.render('calculator.jade'); });
-app.get('/templates/calc.html', function (req, res) { res.render('calc.jade'); });
+app.get('/', function (req, res) { res.render('index.jade'); });
+
+app.get('/templates/calculator.html', function (req, res) {
+  res.render('calculator.jade');
+});
 
 server.listen(9001);
 
