084d3b649f7bf437a6cf7429cd0cd6711d8091e4
Peter MacMillan <peter@macmillan.io>
2014-05-14 10:46:10 -0400

    test memory

--- test/unit/calculator.js
+++ test/unit/calculator.js
@@ -20,5 +20,21 @@ describe('calculator', function () {
 
       expect($scope.value).toEqual(15);
     }));
+
+    it('should remember values', inject(function ($controller) {
+      var $scope = {};
+      var Ctrl = $controller('CalculatorCtrl', { $scope: $scope });
+
+      Ctrl.exec('4');
+      Ctrl.exec('2');
+      Ctrl.exec('M+');
+      Ctrl.exec('C');
+
+      expect($scope.value).toEqual(0);
+
+      Ctrl.exec('MR');
+
+      expect($scope.value).toEqual(42);
+    }));
   });
 });
